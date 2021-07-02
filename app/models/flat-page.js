cdbae4027e7aaeff8ba3b3aa109539b0c118bc96
Jay Varner <jayvarner@gmail.com>
2021-07-02 12:54:56 -0400

    Update flat-page model

--- app/models/flat-page.js
+++ app/models/flat-page.js
@@ -1,18 +1,15 @@
 import DS from 'ember-data';
-import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/string';
-
 const { Model, attr, hasMany } = DS;
 
 export default class FlatPageModel extends Model {
   @attr('string') title;
   @attr('string') slug;
-  @attr('string', { defaultValue: '' }) content; 
+  @attr('string', { defaultValue: '' }) body;
   @hasMany('tour') tours;
   @hasMany('tour-flat-pages') tourFlatPages;
-  
-  @computed('content')
+
   get safeContent() {
-    return new htmlSafe(this.get('content'));
+    return new htmlSafe(this.body);
   }
 }
