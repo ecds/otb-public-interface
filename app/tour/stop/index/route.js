7863e131c6fcb44c236730061d63e68d3beef88b
Jay Varner <jayvarner@gmail.com>
2021-08-25 15:40:35 -0400

    Fix scroll position of stops on mobile between transition

--- app/tour/stop/index/route.js
+++ app/tour/stop/index/route.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class TourStopIndexRoute extends Route {
+  @service router;
+
+  constructor() {
+    super(...arguments);
+
+    this.router.on('routeDidChange', () => {
+      const container = document.getElementById('otb-mobile-tour-stop-container');
+      if (container) container.scrollTop = 0;
+    });
+  }
 }
