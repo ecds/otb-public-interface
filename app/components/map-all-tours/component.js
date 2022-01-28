ae7a9aeb75e24f2cd929500ad4b9e8de7924c7ef
Jay Varner <jayvarner@gmail.com>
2022-01-28 09:53:45 -0500

    Make index map expandable

---
+++ app/components/map-all-tours/component.js
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class MapAllToursComponent extends Component {
+  @tracked
+  mapExpanded = false;
+
+
+}
