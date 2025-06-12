7ab0e659bc58782d9beb19c9a5e2792f2bbcfb0b
Atharva Negi <atharvanegi232@gmail.com>
2025-06-12 18:56:36 -0400

    all the fixes

--- app/routes.ts
+++ app/routes.ts
@@ -5,9 +5,9 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  index("routes/_index.tsx"),
+  index("routes/index.tsx"),
   route("tours", "routes/tours.tsx"),
-  route(":tour", "routes/$tour.tsx", [
-    route(":stop", "routes/$tour.$stop.tsx"),
+  route(":tour", "routes/tour.tsx", [        
+    route(":stop", "routes/stop.tsx"),       
   ]),
 ] satisfies RouteConfig;
